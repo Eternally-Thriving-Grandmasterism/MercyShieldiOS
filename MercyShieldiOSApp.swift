8ca995a9de5300ef71a667b587a4ede0266f79a2
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-11 07:01:37 -0500

    Create MercyShieldiOSApp.swift

---
+++ MercyShieldiOSApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import MercyOS  // Rust PQC bindings via uniffi eternal supreme immaculate
+
+@main
+struct MercyShieldiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
